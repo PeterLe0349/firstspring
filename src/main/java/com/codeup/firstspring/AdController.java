d5d7f81e583416c4cc9018ccb2a1f4534680e1e3
PeterLe0349 <103457196+PeterLe0349@users.noreply.github.com>
2022-07-20 18:56:23 -0500

    update

--- src/main/java/com/codeup/firstspring/AdController.java
+++ src/main/java/com/codeup/firstspring/AdController.java
@@ -1,8 +1,13 @@
 package com.codeup.firstspring;
 
+import com.codeup.firstspring.Models.Ad;
+import com.codeup.firstspring.Models.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 class AdController {
@@ -16,6 +21,13 @@ class AdController {
 
     @GetMapping("/ads")
     public String index(Model model) {
+//        List<Post> posts = new ArrayList<>();
+//        posts.add(new Post("first ad", "some ad first stuff"));
+//        posts.add(new Post("second ad", "some ad second stuff"));
+//        model.addAttribute("ads", posts);
+//        for(Ad a: adDao.findAll()){
+//            System.out.println(a);
+//        }
         model.addAttribute("ads", adDao.findAll());
         return "ads/index";
     }
